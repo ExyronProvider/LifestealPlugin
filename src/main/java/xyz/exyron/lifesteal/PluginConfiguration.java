9f13cf72ca22bd377aa354672336b628c461d129
KinczeK <ls40@onet.pl>
2022-05-09 17:05:44 +0200

    hotfix

--- src/main/java/xyz/exyron/lifesteal/PluginConfiguration.java
+++ src/main/java/xyz/exyron/lifesteal/PluginConfiguration.java
@@ -10,58 +10,58 @@ import xyz.exyron.lifesteal.helpers.StringHelper;
 public class PluginConfiguration extends OkaeriConfig {
 
     @Comment("# Placeholder text (%lifesteal_heartsformatted%)")
-    private final String placeholderFormat = StringHelper.colorize("&c{h}&4❤");
+    private String placeholderFormat = StringHelper.colorize("&c{h}&4❤");
     @Comment("# Should player lose heart/s even if he will not be killed by player?")
-    private final boolean loseHeartsWhatever = false;
+    private boolean loseHeartsWhatever = false;
     @Comment("# Number of hearts gained by kill")
-    private final byte heartsGainByKill = 1;
+    private byte heartsGainByKill = 1;
     @Comment("# Number of hearts lost by death")
-    private final byte heartsLostByDeath = 1;
+    private byte heartsLostByDeath = 1;
     @Comment("# Default number of hearts")
-    private final byte defaultHeartsAmount = 10;
+    private byte defaultHeartsAmount = 10;
     @Comment("# Minimum number of hearts, when player will exceed this minimum server will execute command")
-    private final byte minimumHearts = 2;
+    private byte minimumHearts = 2;
     @Comment("# Maximum number of hearts, player can't have more than that.")
-    private final byte maximumHearts = 30;
+    private byte maximumHearts = 30;
     @Comment("# Permission to /adminhearts command")
-    private final String adminPermision = "lifesteal.adminhearts";
+    private String adminPermision = "lifesteal.adminhearts";
 
     @Comment("# /adminhearts command usage")
-    private final String adminHeartsUsage = StringHelper.colorize("&a/ahearts &2[ info/reset/add/remove/set ] [ player ] ( value )");
+    private String adminHeartsUsage = StringHelper.colorize("&a/ahearts &2[ info/reset/add/remove/set ] [ player ] ( value )");
     @Comment("# Message sent when player provied a wrong amount")
-    private final String wrongAmount = StringHelper.colorize("&cYou provided a wrong amount!");
+    private String wrongAmount = StringHelper.colorize("&cYou provided a wrong amount!");
     @Comment("# Message sent when provided player is offline")
-    private final String targetOffline = StringHelper.colorize("&cProvided player is currently offline!");
+    private String targetOffline = StringHelper.colorize("&cProvided player is currently offline!");
     @Comment("# Message sent when player cannot remove hearts from player ({a} - amount that cannot bed removed)")
-    private final String cannotRemoveHearts = StringHelper.colorize("&aYou cannot remove &2{a} &ahearts from this player, because he's hearts amount will be negative!");
+    private String cannotRemoveHearts = StringHelper.colorize("&aYou cannot remove &2{a} &ahearts from this player, because he's hearts amount will be negative!");
     @Comment("# Message sent when player cannot add hearts to player ({a} - amount that cannot be added)")
-    private final String cannotAddHearts = StringHelper.colorize("&aYou cannot add &2{a} &ahearts to this player, because he's hearts amount will be above the limit!");
+    private String cannotAddHearts = StringHelper.colorize("&aYou cannot add &2{a} &ahearts to this player, because he's hearts amount will be above the limit!");
     @Comment("# Message sent when player executes /adminhearts info (nick) ({p} - player's name, {a} - current amount of hearts)")
-    private final String heartsAmount = StringHelper.colorize("&2{p}'s &ahearts amount is &2{a}");
+    private String heartsAmount = StringHelper.colorize("&2{p}'s &ahearts amount is &2{a}");
     @Comment("# Message sent when player successfully removes amount of hearts from someone ({a} - amount of removed hearts, {p} - player's name)")
-    private final String successfullyRemovedHearts = StringHelper.colorize("&aSucessfully removed {a} hearts from player {p}");
+    private String successfullyRemovedHearts = StringHelper.colorize("&aSucessfully removed {a} hearts from player {p}");
     @Comment("# Message send when player successfully adds amount of hearts to someone ({a} - amount of added hearts, {p} - player's name)")
-    private final String successfullyAddedHearts = StringHelper.colorize("&aSucessfully added &2{a} &ahearts to player &2{p}");
+    private String successfullyAddedHearts = StringHelper.colorize("&aSucessfully added &2{a} &ahearts to player &2{p}");
     @Comment("# Message sent when player successfully reset someone's hearts amount to default value ({p} - player's name, {a} - default amount of hearts)")
-    private final String successfullyResetHearts = StringHelper.colorize("&aYou sucessfully reset &2{p}'s &ahearts amount to &2{a}");
+    private String successfullyResetHearts = StringHelper.colorize("&aYou sucessfully reset &2{p}'s &ahearts amount to &2{a}");
     @Comment("# Message sent when player successfully set someone's hearts amount to value ({p} - player's name, {a} - amount of hearts set)")
-    private final String setHeartsMessage = StringHelper.colorize("&aYou successfully set &2{p} &ahearts amount to &2{a}");
+    private String setHeartsMessage = StringHelper.colorize("&aYou successfully set &2{p} &ahearts amount to &2{a}");
     @Comment("# Message sent on /hearts command ({h} - player's hearts amount)")
-    private final String heartsCommandMessage = StringHelper.colorize("&aYou currently have &2{h} &ahearts!");
+    private String heartsCommandMessage = StringHelper.colorize("&aYou currently have &2{h} &ahearts!");
     @Comment("# Message sent when player got killed by other player ({h} - hearts amount, {p} - killer name)")
-    private final String messageWhenPlayerGotKilledByPlayer = StringHelper.colorize("&cYou lost &4{h} &aheart, because &4{p}&c killed you!");
+    private String messageWhenPlayerGotKilledByPlayer = StringHelper.colorize("&cYou lost &4{h} &cheart, because &4{p}&c killed you!");
     @Comment("# Message sent when player kills somone ({p} - killed player name, {h} - amount of hearts gained)")
-    private final String messageWhenPlayerKillsSomeone = StringHelper.colorize("&cYou just killed &4{p}&c, and got &4{h} &chearts from him!");
+    private String messageWhenPlayerKillsSomeone = StringHelper.colorize("&cYou just killed &4{p}&c, and got &4{h} &chearts from him!");
     @Comment("# Message sent when player got killed by world ({h} - hearts amount)")
-    private final String messageWhenPlayerGotKilledByWorld = StringHelper.colorize("&cYou lost &4{h} &cheart, because something killed you!");
+    private String messageWhenPlayerGotKilledByWorld = StringHelper.colorize("&cYou lost &4{h} &cheart, because something killed you!");
     @Comment("# Message sent when player has the minimum amount of hearts")
-    private final String minimumHeartsMessage = StringHelper.colorize("&cHearts were not taken away from you because you reached the minimum number of hearts!");
+    private String minimumHeartsMessage = StringHelper.colorize("&cHearts were not taken away from you because you reached the minimum number of hearts!");
     @Comment("# Message sent when player do not have permission to use command")
-    private final String noPermissionMessage = StringHelper.colorize("&cYou don't have permision to use this command!");
+    private String noPermissionMessage = StringHelper.colorize("&cYou don't have permision to use this command!");
     @Comment("# Message sent when player has the maximum amount of hearts")
-    private final String maximumHeartsMessage = StringHelper.colorize("&cHearts have not been added because you have reached the maximum number of hearts!");
+    private String maximumHeartsMessage = StringHelper.colorize("&cHearts have not been added because you have reached the maximum number of hearts!");
     @Comment("# Command executed when player reaches the minimum amount of hearts ({p} - player)")
-    private final String lessOrEquallyHeartsThanMinimumCommand = "ban {p} You have reached a minimum amount of hearts, cya!";
+    private String lessOrEquallyHeartsThanMinimumCommand = "ban {p} You have reached a minimum amount of hearts, cya!";
 
     public String getPlaceholderFormat() {
         return placeholderFormat;
--- src/main/java/xyz/exyron/lifesteal/handlers/PlayerDeathHandler.java
+++ src/main/java/xyz/exyron/lifesteal/handlers/PlayerDeathHandler.java
@@ -8,8 +8,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import xyz.exyron.lifesteal.PluginConfiguration;
 
-import java.util.Objects;
-
 public class PlayerDeathHandler implements Listener {
     private final PluginConfiguration configuration;
 
@@ -27,26 +25,26 @@ public class PlayerDeathHandler implements Listener {
             if ((killer.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() / 2) >= configuration.getMaximumHearts()) {
                 killer.sendMessage(configuration.getMaximumHeartsMessage());
             } else {
-                Objects.requireNonNull(killer.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(killer.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() + (configuration.getHeartsGainByKill() * 2));
+                killer.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(killer.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() + (configuration.getHeartsGainByKill() * 2));
                 killer.sendMessage(configuration.getMessageWhenPlayerKillsSomeone().replace("{p}", player.getName()).replace("{h}", String.valueOf(configuration.getHeartsGainByKill())));
             }
 
             if ((player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() / 2) <= configuration.getMinimumHearts() || player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() - (configuration.getHeartsLostByDeath() * 2) < 1) {
                 player.sendMessage(configuration.getMinimumHeartsMessage());
-                Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(configuration.getDefaultHeartsAmount() * 2);
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(configuration.getDefaultHeartsAmount() * 2);
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), configuration.getLessOrEquallyHeartsThanMinimumCommand().replace("{p}", player.getName()));
             } else {
-                Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() - (configuration.getHeartsLostByDeath() * 2));
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() - (configuration.getHeartsLostByDeath() * 2));
                 player.sendMessage(configuration.getMessageWhenPlayerGotKilledByPlayer().replace("{p}", killer.getName()).replace("{h}", String.valueOf(configuration.getHeartsLostByDeath())));
             }
 
         } else if (configuration.isLoseHeartsWhatever()) {
             if ((player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() / 2) <= configuration.getMinimumHearts() || player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() - (configuration.getHeartsLostByDeath() * 2) < 1) {
-                Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(configuration.getDefaultHeartsAmount() * 2);
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(configuration.getDefaultHeartsAmount() * 2);
                 player.sendMessage(configuration.getMinimumHeartsMessage());
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(), configuration.getLessOrEquallyHeartsThanMinimumCommand().replace("{p}", player.getName()));
             } else {
-                Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() - (configuration.getHeartsLostByDeath() * 2));
+                player.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() - (configuration.getHeartsLostByDeath() * 2));
                 player.sendMessage(configuration.getMessageWhenPlayerGotKilledByWorld().replace("{h}", String.valueOf(configuration.getHeartsLostByDeath())));
             }
         }
